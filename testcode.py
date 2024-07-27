86c025d0d68159e00d53dc87a1627cd459729ecb
dima19-93 <dima70593@ukr.net>
2024-07-27 12:31:26 +0300

    Update testcode.py
    Add programme

--- testcode.py
+++ testcode.py
@@ -1,7 +1,21 @@
-print('Hello friend!') 
-#
-#
-#
-'''     '''
-print('END')
-'''    '''
+import psutil
+
+def get_system_info():
+    # Свободное место на диске (в байтах)
+    disk_free = psutil.disk_usage('/').free
+    # Размер своп-диска (в байтах)
+    swap_size = psutil.swap_memory().total
+    # Занятая оперативная память (в байтах)
+    ram_used = psutil.virtual_memory().used
+    # Список запущенных процессов
+    running_processes = psutil.process_iter()
+
+    print(f"Свободное место на диске: {disk_free} байт")
+    print(f"Размер своп-диска: {swap_size} байт")
+    print(f"Занятая оперативная память: {ram_used} байт")
+    print("Запущенные процессы:")
+    for process in running_processes:
+        print(f"- {process.name()}")
+
+if __name__ == "__main__":
+    get_system_info()
